6cbb6997183cfa12bedcd8d1fafa3cc8c79db1de
Anderson Borges <126627528+Arqueon01@users.noreply.github.com>
2023-03-05 16:46:00 -0300

    Update zenny.py
    Função que ao digitar !mu + nome do canal de voz ele marca todos que estão no canal de voz

--- zenny.py
+++ zenny.py
@@ -7,11 +7,11 @@ intents.presences = False
 intents.members = True
 intents.message_content = True
 
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
-        async def mark_users(ctx, voice_channel_name):
-            await mark_all_users(ctx, voice_channel_name)
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
@@ -24,7 +24,17 @@ class MyClient(discord.Client):
                 await asyncio.sleep(time)
                 await message.channel.send(f'{message.author.mention}, Acabou a tranquilidade de {time} segundos.')
             except:
-                await message.channel.send('Uso correto: !timer <tempo em segundos>')
+                await message.channel.send('Uso correto: !timer <tempo em minutos>')
+        elif  message.content.startswith('!mu'):
+                voice_channel_name = message.content.split()[1]
+                voice_channel = discord.utils.get(message.guild.voice_channels, name=voice_channel_name)
+                if voice_channel and len(voice_channel.members) > 0:
+                    mentions = [member.mention for member in voice_channel.members]
+                    response = " ".join(mentions) + "hora de passar raiva!"
+                    await message.channel.send(response)
+                else:
+                    response = "Nenhum usuário está conectado ao canal de voz especificado."
+                    await message.channel.send(response)
 
 client = MyClient(intents=intents)
 client.run('TOKEN')
